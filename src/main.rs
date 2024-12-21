cddf85bd0c2935aee22a7c5d71f9f357e5821df9
Tokeiya <netseed@seedcreate.onmicrosoft.com>
2024-12-21 15:09:35 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,19 @@ mod location_resolver;
 mod platform;
 mod platform_load_error;
 
+use std::fs;
+
 fn main() {
 	let a = location_resolver::get_folder();
 	println!("{a:?}");
 
 	println!("{:?}", location_resolver::get_config_path());
 	println!("{:?}", location_resolver::get_node_path());
+
+	let file = fs::File::open(location_resolver::get_config_path().unwrap()).unwrap();
+	let conf = platform::Platform::from_read(file);
+
+	if let Err(e) = conf {
+		println!("{e:?}")
+	}
 }
